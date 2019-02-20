2990900f75898a2db7e5865f3ff87b7eb911eb5d
Ubirajara Rodrigues de Barros <ubirajararodrigues@msn.com>
2019-02-20 08:13:06 -0300

    test on python

---
+++ examples/python/src/class/MyStack.py
@@ -0,0 +1,34 @@
+class MyStack:
+    def __init__(self, size):
+        self.maxSize = size
+        self.items = []
+    
+    def push(self, data):
+        self.items.append(data)
+
+    def pop(self):
+        return self.items.pop()
+    
+    def peek(self):
+        if self.items:
+            return self.items[-1]
+    
+    def isEmpty(self):
+        return self.items == []
+
+    def isFull(self):
+        return (self.items.__len__() == (self.maxSize - 1))
+
+if __name__ == '__main__':
+    s = MyStack(5)
+    s.push(10)
+    s.push(20)
+    s.push(30)
+    s.push(40)
+    s.push(50)
+
+    while (s.isEmpty() == False):
+        print(s.pop())
+        print(' ')
+
+    print ('end')
---
+++ examples/python/src/class/MyStackTest.py
@@ -0,0 +1,14 @@
+import unittest
+from MyStack import MyStack
+
+class MyStackTest(unittest.TestCase):
+    def setUp(self):
+        self.stack = MyStack(5)
+
+    def shouldInsertAndRemoveWithoutProblem(self):
+        self.stack.push(100)
+        self.assertEquals(self.stack.pop(), 100)
+        self.stack.push(200)
+        self.stack.push(100)
+        self.stack.push(50)
+        self.assertEquals(self.stack.pop(), 100)
