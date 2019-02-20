5b5c62451536ed0de7bb00500c5a5cfff718a132
Ubirajara Rodrigues de Barros <ubirajararodrigues@msn.com>
2019-02-20 08:13:59 -0300

    class test - python

--- examples/python/src/class/MyStackTest.py
+++ examples/python/src/class/MyStackTest.py
@@ -5,7 +5,7 @@ class MyStackTest(unittest.TestCase):
     def setUp(self):
         self.stack = MyStack(5)
 
-    def shouldInsertAndRemoveWithoutProblem(self):
+    def test_shouldInsertAndRemoveWithoutProblem(self):
         self.stack.push(100)
         self.assertEquals(self.stack.pop(), 100)
         self.stack.push(200)
